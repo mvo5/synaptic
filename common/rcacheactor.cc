49e1e57079dca38dba0755b0c2a2ec986359b3b3
niemeyer <>
2003-03-16 02:07:30 -0300

    Forgot to include rcacheactor.*.

---
+++ common/rcacheactor.cc
@@ -0,0 +1,183 @@
+
+#include "rcacheactor.h"
+#include "i18n.h"
+
+#include <apt-pkg/error.h>
+#include <apt-pkg/tagfile.h>
+#include <apt-pkg/strutl.h>
+#include <apt-pkg/configuration.h>
+
+#include <fnmatch.h>
+
+void RCacheActor::notifyCachePostChange()
+{
+   vector<RPackage*> toKeep;
+   vector<RPackage*> toInstall; 
+   vector<RPackage*> toUpgrade; 
+   vector<RPackage*> toRemove;
+   vector<RPackage*> exclude; // empty
+
+   if (_lister->getStateChanges(*_laststate, toKeep, toInstall,
+			        toUpgrade, toRemove, exclude, false)) {
+      if (toKeep.empty() == false)
+	 run(toKeep, ACTION_KEEP);
+      if (toInstall.empty() == false)
+	 run(toInstall, ACTION_INSTALL);
+      if (toUpgrade.empty() == false)
+	 run(toUpgrade, ACTION_INSTALL);
+      if (toRemove.empty() == false)
+	 run(toRemove, ACTION_REMOVE);
+   }
+}
+
+RCacheActorRecommends::RCacheActorRecommends(RPackageLister *lister,
+					     string FileName)
+      : RCacheActor(lister)
+{
+   FileFd F(FileName, FileFd::ReadOnly);
+   if (_error->PendingError()) 
+   {
+      _error->Error(_("could not open recommends file %s"),
+		    FileName.c_str());
+      return;
+   }
+   pkgTagFile Tags(&F);
+   pkgTagSection Section;
+
+   string Name;
+   string Match;
+   string Recommends;
+   while (Tags.Step(Section)) {
+      string Name = Section.FindS("Name");
+      string Match = Section.FindS("Match");
+      string Recommends = Section.FindS("Recommends");
+      if (Name.empty() == true || Recommends.empty() == true)
+	 continue;
+
+      ListType *List = NULL;
+      if (Match.empty() == true || Match == "exact") {
+	 List = &_map[Name];
+      } else if (Match == "wildcard") {
+	 List = &_map_wildcard[Name];
+      } else if (Match == "regex") {
+	 regex_t *ptrn = new regex_t;
+	 if (regcomp(ptrn,Name.c_str(),
+		     REG_EXTENDED|REG_ICASE|REG_NOSUB) != 0)
+	 {
+	    _error->Warning(_("Bad regular expression '%s' in Recommends file."),
+			    Name.c_str());
+	    delete ptrn;
+	 }
+	 else
+	    List = &_map_regex[ptrn];
+      }
+
+      if (List != NULL) {
+	 const char *C = Recommends.c_str();
+	 string S;
+	 while (*C != 0)
+	 {
+	    if (ParseQuoteWord(C,S))
+	       List->push_back(S);
+	 }
+      }
+   }
+}
+
+RCacheActorRecommends::~RCacheActorRecommends()
+{
+   for (RegexMapType::const_iterator RMapI = _map_regex.begin();
+	RMapI != _map_regex.end(); RMapI++) {
+      delete RMapI->first;
+   }
+}
+
+void RCacheActorRecommends::setLanguageCache()
+{
+   string LangList = _config->Find("Volatile::Languages", "");
+   if (LangList == _langLast)
+      return;
+
+   _langCache.clear();
+
+   if (LangList.empty())
+      return;
+
+   _langLast = LangList;
+
+   const char *C = LangList.c_str();
+   string S;
+   while (*C != 0) {
+      if (ParseQuoteWord(C,S)) {
+	 string::size_type end;
+	 end = S.find('@');
+	 if (end != string::npos)
+	    S.erase(end, string::npos);
+	 _langCache.push_back(S);
+	 end = S.rfind('_');
+	 if (end != string::npos) {
+	    S.erase(end, string::npos);
+	    _langCache.push_back(S);
+	 }
+      }
+   }
+}
+
+void RCacheActorRecommends::notifyCachePostChange()
+{
+   if (_config->FindB("Synaptic::UseRecommends", true) == true)
+      RCacheActor::notifyCachePostChange();
+}
+
+void RCacheActorRecommends::run(vector<RPackage*> &PkgList, int Action)
+{
+   setLanguageCache();
+   RPackage *Pkg;
+   const char *name;
+   const ListType *List;
+   MapType::const_iterator MapI;
+   for (vector<RPackage*>::const_iterator PkgI = PkgList.begin();
+         PkgI != PkgList.end(); PkgI++) {
+      name = (*PkgI)->name();
+      List = NULL;
+      MapI = _map.find(name);
+      if (MapI != _map.end()) {
+	 List = &MapI->second;
+      } else {
+	 if (_map_wildcard.empty() == false) {
+	    for (MapI = _map_wildcard.begin();
+		 MapI != _map_wildcard.end(); MapI++) {
+	       if (fnmatch(MapI->first.c_str(), name, 0) == 0) {
+		  List = &MapI->second;
+		  break;
+	       }
+	    }
+	 }
+	 if (List == NULL && _map_regex.empty() == false) {
+	    for (RegexMapType::const_iterator RMapI = _map_regex.begin();
+		 RMapI != _map_regex.end(); RMapI++) {
+	       if (regexec(RMapI->first, name, 0, 0, 0) == 0) {
+		  List = &RMapI->second;
+		  break;
+	       }
+	    }
+	 }
+      }
+      if (List != NULL) {
+	 for (ListType::const_iterator ListI = List->begin();
+	      ListI != List->end(); ListI++) {
+	    const string &Recommends = *ListI;
+	    if (actOnPkg(Recommends, Action) == false
+	        && Recommends.find("$(LANG)") != string::npos) {
+	       for (vector<string>::const_iterator LI = _langCache.begin();
+		    LI != _langCache.end(); LI++) {
+		  string Parsed = SubstVar(Recommends, "$(LANG)", *LI);
+		  actOnPkg(Parsed, Action);
+	       }
+	    }
+	 }
+      }
+   }
+}
+
+// vim:sts=3:sw=3
---
+++ common/rcacheactor.h
@@ -0,0 +1,128 @@
+/* rcacheactor.h
+ * 
+ * Copyright (c) 2000-2003 Conectiva S/A 
+ *               2002 Michael Vogt <mvo@debian.org>
+ * 
+ * Author: Alfredo K. Kojima <kojima@conectiva.com.br>
+ *         Michael Vogt <mvo@debian.org>
+ * 
+ * This program is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License as 
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA
+ */
+
+#ifndef RCACHEACTOR_H
+#define RCACHEACTOR_H
+
+#include "rpackagelister.h"
+
+#include <regex.h>
+
+class RCacheActor : public RCacheObserver
+{
+   public:
+
+   enum Action {
+      ACTION_KEEP,
+      ACTION_INSTALL,
+      ACTION_REMOVE
+   };
+
+   protected:
+
+   RPackageLister *_lister;
+   RPackageLister::pkgState *_laststate;
+
+   inline bool actOnPkg(string Name, int Action)
+   {
+      RPackage *Pkg = _lister->getElement(Name);
+      if (Pkg != NULL) {
+	 switch (Action) {
+	    case ACTION_KEEP:
+	       Pkg->setKeep();
+	       break;
+	    case ACTION_INSTALL:
+	       Pkg->setInstall();
+	       break;
+	    case ACTION_REMOVE:
+	       Pkg->setRemove();
+	       break;
+	 }
+	 return true;
+      }
+      return false;
+   };
+
+   public:
+
+   virtual void run(vector<RPackage*> &List, int Action) = 0;
+
+   virtual void notifyCachePreChange()
+   {
+      updateState();
+   };
+
+   virtual void notifyCachePostChange();
+
+   virtual void notifyCacheOpen() {};
+
+   virtual void updateState()
+   {
+     delete _laststate;
+     _laststate = new RPackageLister::pkgState;
+     _lister->saveState(*_laststate);
+   };
+
+   RCacheActor(RPackageLister *lister)
+	 : _lister(lister), _laststate(0)
+   {
+      _lister->registerCacheObserver(this);
+   };
+
+   virtual ~RCacheActor()
+   {
+      _lister->unregisterCacheObserver(this);
+   };
+};
+
+class RCacheActorRecommends : public RCacheActor
+{
+   protected:
+
+   typedef vector<string> ListType;
+   typedef map<string,ListType> MapType;
+   typedef map<regex_t*,ListType> RegexMapType;
+   
+   MapType _map;
+   MapType _map_wildcard;
+   RegexMapType _map_regex;
+
+   string _langLast;
+   ListType _langCache;
+
+   void setLanguageCache();
+
+   public:
+
+   virtual void run(vector<RPackage*> &List, int Action);
+
+   virtual void notifyCachePostChange();
+
+   RCacheActorRecommends(RPackageLister *lister, string FileName);
+   virtual ~RCacheActorRecommends();
+};
+
+#endif
+
+// vim:sts=3:sw=3

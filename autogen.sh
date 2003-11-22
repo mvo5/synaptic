be7e60aab7f91cb3f2b9401bf81be7d3e9e49dbc
niemeyer <>
2003-11-22 06:51:02 -0300

    Fixing automake version in autogen.sh.

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
 
+REQUIRED_AUTOMAKE_VERSION=1.7
+
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
@@ -18,4 +20,4 @@ which gnome-autogen.sh || {
     echo "You need to install gnome-common from the GNOME CVS"
     exit 1
 }
-USE_GNOME2_MACROS=1 . gnome-autogen.sh
+USE_GNOME2_MACROS=1 .  gnome-autogen.sh

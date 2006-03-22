337a5312f6584e4eb98f58c99e1f31ac07ccacae
Michael Vogt <michael.vogt@ubuntu.com>
2006-03-23 00:26:56 +0100

    * fix compile error

--- gtk/rguserdialog.cc
+++ gtk/rguserdialog.cc
@@ -221,8 +221,8 @@ bool RGGladeUserDialog::init(const char *name)
    int id = _config->FindI("Volatile::ParentWindowId", -1);
    if (id > 0) {
       GdkWindow *win = gdk_window_foreign_new(id);
-      gtk_widget_realize(dia);
-      gdk_window_set_transient_for(GDK_WINDOW(dia->window), win);
+      gtk_widget_realize(_dialog);
+      gdk_window_set_transient_for(GDK_WINDOW(_dialog->window), win);
    }
 
    g_free(filename);

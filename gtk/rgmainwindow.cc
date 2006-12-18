20167b37fdc46a0258b21b994e7742ab5e4a1ce3
Michael Vogt <egon@bottom>
2006-12-18 10:52:25 +0100

    * fix crash

--- gtk/rgmainwindow.cc
+++ gtk/rgmainwindow.cc
@@ -2895,7 +2895,6 @@ void RGMainWindow::cbMenuPinClicked(GtkWidget *self, void *data)
       return;
 
    me->setInterfaceLocked(TRUE);
-   me->setTreeLocked(TRUE);
    me->_lister->unregisterObserver(me);
 
    // save to temporary file
@@ -2921,6 +2920,7 @@ void RGMainWindow::cbMenuPinClicked(GtkWidget *self, void *data)
       _roptions->setPackageLock(pkg->name(), active);
       li = g_list_next(li);
    }
+   me->setTreeLocked(TRUE);
    me->_lister->openCache();
 
    // reread saved selections

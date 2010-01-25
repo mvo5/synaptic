91e3a3e1fad1b86bc6d862bc2c503442d421fbee
Michael Vogt <michael.vogt@ubuntu.com>
2010-01-25 11:28:59 +0100

    add tests/test_rpackageview.cc with basic profiling

---
+++ tests/test_rpackageview.cc
@@ -0,0 +1,24 @@
+#include <apt-pkg/init.h>
+#include <iostream>
+
+#include "config.h"
+#include "rpackagelister.h"
+#include "rpackageview.h"
+#include "rpackage.h"
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+   pkgInitConfig(*_config);
+   pkgInitSystem(*_config, _system);
+   
+   RPackageLister *lister = new RPackageLister();
+   lister->openCache();
+   lister->setView(PACKAGE_VIEW_SEARCH);
+   OpProgress progress;
+
+   unsigned long now = clock();
+   lister->searchView()->setSearch("synaptic",  RPatternPackageFilter::Description, "synaptic", progress);
+   cerr << "searching: " << float(clock()-now)/CLOCKS_PER_SEC << endl;
+}

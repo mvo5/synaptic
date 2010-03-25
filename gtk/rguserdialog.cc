53996a61bc55b3c251d88201f6ff62f800f8851e
Michael Vogt <egon@pop>
2010-03-25 21:34:44 +0100

    * wrap lines in textarea of dialog_update_failed (LP: #237455) * set textarea read-only in generic error dialog (LP: #403100) * Change 'Icon Legend' dialog to fixed size (LP: #374376) * add tooltip to 'properties' and 'search' buttons (LP: #202681)

--- gtk/rguserdialog.cc
+++ gtk/rguserdialog.cc
@@ -86,6 +86,7 @@ bool RGUserDialog::showErrors()
    gtk_text_view_set_wrap_mode(GTK_TEXT_VIEW(textview), GTK_WRAP_WORD);
    gtk_text_view_set_left_margin(GTK_TEXT_VIEW(textview), 3);
    gtk_text_view_set_cursor_visible(GTK_TEXT_VIEW(textview), FALSE);
+   gtk_text_view_set_editable(GTK_TEXT_VIEW(textview), FALSE);
    gtk_scrolled_window_set_shadow_type(GTK_SCROLLED_WINDOW(scroll), 
                                        GTK_SHADOW_IN);
    gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scroll),

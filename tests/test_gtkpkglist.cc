c79b81229408159a8c28d8271228691cd792be08
Michael Vogt <michael.vogt@ubuntu.com>
2014-03-16 09:37:50 +0100

    tests/test_gtkpkglist.cc: improve test

--- tests/test_gtkpkglist.cc
+++ tests/test_gtkpkglist.cc
@@ -22,16 +22,35 @@ int main(int argc, char **argv)
    pkgInitConfig(*_config);
    pkgInitSystem(*_config, _system);
    
+   _config->Set("Debug::Synaptic::View", true);
+   
    RPackageLister *lister = new RPackageLister();
    lister->openCache();
+   lister->setView(0);
+   lister->setSubView("");
+
+   std::cerr << "lister all packages size: " 
+             << lister->packagesSize()
+             << std::endl;
+
+   std::cerr << "lister visible size: " 
+             << lister->viewPackagesSize()
+             << std::endl;
 
    GtkTreeModel *pkglist = GTK_TREE_MODEL(gtk_pkg_list_new(lister));
    GtkWidget *treeview = gtk_tree_view_new();
    setupTreeView(treeview);
    gtk_tree_view_set_model(GTK_TREE_VIEW(treeview), pkglist);
 
+   std::cerr << "size model: " 
+             << gtk_tree_model_iter_n_children(pkglist, NULL) 
+             << std::endl;
+
+   GtkWidget *scroll =  gtk_scrolled_window_new(NULL, NULL);
+   gtk_container_add(GTK_CONTAINER(scroll), treeview);
+
    GtkWidget *win = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-   gtk_container_add(GTK_CONTAINER(win), treeview);
+   gtk_container_add(GTK_CONTAINER(win), scroll);
    gtk_window_set_default_size(GTK_WINDOW(win), 600, 400);
    gtk_widget_show_all(win);
 

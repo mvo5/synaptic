6732babdfab3ef838fe946735a17da20aba5c597
Amaa <Amaa>
2026-07-01 19:26:40 +0000

    Add missing inclusion of apt-pkg/macros.h for APT_PKG_MAJOR definition: Added the missing inclusion of apt-pkg/macros.h and revised the other inclusions.

--- common/pkg_acqfile.h
+++ common/pkg_acqfile.h
@@ -6,12 +6,21 @@
 
 #include "config.h"
 
-#include <apt-pkg/acquire-item.h>
+#include <apt-pkg/macros.h>   // Provides the definition of APT_PKG_MAJOR.
+
+#if APT_PKG_MAJOR >= 5
 
 // new APT has a proper pkgAcqFile so all good
-#if APT_PKG_MAJOR >= 5
- #define pkgAcqFileSane pkgAcqFile
+#include <apt-pkg/acquire-item.h>
+#define pkgAcqFileSane pkgAcqFile
+
 #else
+
+#include <apt-pkg/acquire.h>
+#include <apt-pkg/acquire-item.h>
+
+#include <string>
+
 class pkgAcqFileSane : public pkgAcquire::Item
 // This is frustrating: pkgAcqFile is **almost** good enough, but has some
 // hardcoded stuff that makes it not quite work.
@@ -27,7 +36,7 @@ public:
                  std::string URI,
                  HashStringList const &hsl,
                  unsigned long long const Size,
-		 std::string Description,
+                 std::string Description,
                  std::string ShortDesc,
                  std::string const &DestDir,
                  std::string const &DestFilename);
@@ -37,6 +46,7 @@ public:
   std::string DescURI() {return Desc.URI;}
   virtual ~pkgAcqFileSane() {}
 };
-#endif
+
+#endif // APT_PKG_MAJOR >= 5
 
 #endif

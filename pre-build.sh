fecc84e2cef8832f5a05e0209b36f662990b77c0
Andrey Kutejko <andy128k@gmail.com>
2026-07-12 13:41:17 +0200

    Update pre-build.sh to modify meson.build

--- pre-build.sh
+++ pre-build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 DEBVER=$(dpkg-parsechangelog |sed -n -e '/^Version:/s/^Version: //p')
-CONFVER=$(sed -n -e 's/AC_INIT(synaptic, \(.*\))/\1/p' configure.ac)
+CONFVER=$(grep -oP "version:\s*'\K[^']+" meson.build | head -1)
 if [ "$DEBVER" != "$CONFVER" ]; then
-	sed -i -e "s/$CONFVER/$DEBVER/" configure.ac
-	echo "Changed version in configure.ac, please commit."
+	sed -i -e "s/$CONFVER/$DEBVER/" meson.build
+	echo "Changed version in meson.build, please commit."
 	exit 1
 fi
 
